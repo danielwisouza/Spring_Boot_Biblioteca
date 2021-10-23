228ac371fc4528893365c270cb4ce40f70732e13
danielwisouza <daniel.wis@outlook.com>
2021-10-23 00:07:42 +0000

    Subindo com erro

--- src/main/java/br/gov/sp/fatec/biblioteca/controller/ViewLivro.java
+++ src/main/java/br/gov/sp/fatec/biblioteca/controller/ViewLivro.java
@@ -7,3 +7,10 @@ public class ViewLivro {
     public static class LivroCompleto extends LivroSimplificado {};
     
 }
+/* Exemplo de Post
+
+{"titulo": "O Hobbit", "isbn": "1", "papel": "Folhas de mandrágora"}
+
+{"titulo": "Narnia 2", "isbn": "6", "papel": "Folhas de mandrágora"}
+
+*/
